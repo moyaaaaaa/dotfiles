e7fa7cc6fec945535e5dc06b73850ee21dd921d1
moyaaaaaa <danmakubi@gmail.com>
2017-06-11 15:36:54 +0900

    フレーム移動系キーバインド追加

---
+++ .emacs.d/inits/99_keybindings.el
@@ -0,0 +1,4 @@
+(global-set-key (kbd "C-<left>")  'windmove-left)
+(global-set-key (kbd "C-<down>")  'windmove-down)
+(global-set-key (kbd "C-<up>")    'windmove-up)
+(global-set-key (kbd "C-<right>") 'windmove-right)
