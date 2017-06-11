de1b9ad6bea8f8e03b72fa387b4233f53719f23e
ohmae <ohmae@synchro-food.co.jp>
2017-06-12 08:51:14 +0900

    home

---
+++ .emacs.d/.cask/25.2/elpa/package+-20150319.1455/package+-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "package+" "20150319.1455" "Extensions for the package library." 'nil :commit "4a9618a44ec4f26a14e0136cd9d3c4855fceb25b" :url "TBA" :keywords '("extensions" "tools"))
