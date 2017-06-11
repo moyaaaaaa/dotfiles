bdbdf722572cdb8ae4ba6692857fc75dcb69870e
moyaaaaaa <danmakubi@gmail.com>
2017-06-11 16:29:56 +0900

    フォント系設定追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,11 @@
 ;; ~/.emacs.d/site-lisp 以下全部読み込み
+(package-initialize)
 (let ((default-directory (expand-file-name "~/.emacs.d/site-lisp")))
   (add-to-list 'load-path default-directory)
   (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
       (normal-top-level-add-subdirs-to-load-path)))
 
+;; initsディレクトリ以下の設定ファイルを読み込む
 (require 'init-loader)
 (setq init-loader-show-log-after-init nil)
 (init-loader-load "~/.emacs.d/inits")
