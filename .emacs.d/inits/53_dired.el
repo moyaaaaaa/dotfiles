f2c86ee656c5f956aec9b05d6ce40c3d13e9bb86
moyaaaaaa <danmakubi@gmail.com>
2017-06-11 17:06:36 +0900

    dired追加

---
+++ .emacs.d/inits/53_dired.el
@@ -0,0 +1,13 @@
+;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (dired+ recentf-ext package-utils init-loader helm yasnippet web-mode use-package smex smartparens projectile prodigy popwin pallet nyan-mode multiple-cursors magit idle-highlight-mode htmlize flycheck-cask expand-region exec-path-from-shell elscreen drag-stuff))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
