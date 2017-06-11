f743f871cb4f6cb8cfb25c0fb63befc422b83e3a
moyaaaaaa <danmakubi@gmail.com>
2017-06-11 17:25:12 +0900

    meghanada追加

---
+++ .emacs.d/inits/54_meghanada.el
@@ -0,0 +1,8 @@
+(require 'meghanada)
+(add-hook 'java-mode-hook
+          (lambda ()
+            ;; meghanada-mode on
+            (meghanada-mode t)
+            (setq c-basic-offset 2)
+            ;; use code format
+            (add-hook 'before-save-hook 'meghanada-code-beautify-before-save)))
